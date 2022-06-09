d29d6c94427b47fbbb43aaf413891a9cbad49578
udith355 <udith.19@cse.mrt.ac.lk>
2022-06-09 11:32:39 +0530

    removed unwanted codes

--- obj_detection.py
+++ obj_detection.py
@@ -4,7 +4,6 @@ from imutils import paths
 import numpy as np
 import numpy as np
 
-# import distance_to_camera as dc
 #
 # input sample video file name
 #
@@ -82,7 +81,6 @@ def ObjectDetector(image):
     classes, scores, boxes = model.detect(image, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
     for (classid, score, box) in zip(classes, scores, boxes):
         color = COLORS[int(classid) % len(COLORS)]
-        # print("score : ",score)
         label = "%s" % (class_names[classid])
 
         cv.rectangle(image, box, color, 2)
@@ -93,58 +91,25 @@ def ObjectDetector(image):
     KNOWN_WIDTH = 6.0
     focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
 
-    print("Focal Length : ", focalLength)
     # =======================================================================================================
     for i, b in enumerate(boxes):
-        print(boxes)
 
         if classes[i] == 2 or classes[i] == 7 or classes[i] == 8:
             if scores[i] >= 0.6:
                 apx_distance = distance_to_camera(KNOWN_WIDTH, focalLength, abs(boxes[i][1] - boxes[i][3]))
-                print("Distance : ", apx_distance)
                 mid_x = (boxes[i][1] + boxes[i][3]) / 2
 
                 mid_y = (boxes[i][0] + boxes[i][2]) / 2
 
                 apx_distance = round(((1 - (boxes[i][3] - boxes[i][1]))), 1)
-                print("print 1:", ((boxes[i][1])))
-                print("print 2", ((boxes[i][3])))
-                print("print 3:", ((boxes[i][3] - boxes[i][1])))
-                print("print 4:", (1 - (boxes[i][3] - boxes[i][1])))
-                print("apro distance: ", apx_distance)
-                # cv.putText(image, '{}'.format(apx_distance), (int(mid_x * 800), int(mid_y * 450)),cv.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
 
             if (apx_distance) <= 350:
                 if (mid_x) > (100) and (mid_x) < (400):
-                    cv.putText(image, 'WARNING!!! {apx_distance}'.format(apx_distance=apx_distance),
+                    cv.putText(image, 'WARNING!!!'.format(apx_distance=apx_distance),
                                (box[0] + 10, box[1] + 50), cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 2)
                     cv.rectangle(image, box, (0, 0, 255), 2)
-            # if (apx_distance) <=50:
-            #        if (mid_x) >(100)  and (mid_x) < (400):
-            #            cv.putText(image, 'WARNING!!! {apx_distance}'.format(apx_distance = apx_distance),(box[0]+10, box[1]+50), cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 2)
-            #            cv.rectangle(image, box, (0,0,255), 2)
-    # return   boxes,classes,scores,image
 
 
-# Detect distance of the objects
-def Detect_Distance(boxes, classes, scores, image_np):
-    for i, b in enumerate(boxes):
-        if classes[i] == 2 or classes[i] == 7 or classes[i] == 8:
-            if scores[i] >= 0.5:
-                # print("type :",type(boxes[0][1]))
-                mid_x = (boxes[i][1] + boxes[i][3]) / 2
-                print("Mid_x", mid_x / 1000)
-
-                mid_y = (boxes[i][0] + boxes[i][2]) / 2
-                print("Mid_y", mid_y / 1000)
-                apx_distance = round(((1 - (boxes[i][3] - boxes[i][1]))), 1)
-                print("apro distance: ", apx_distance)
-                cv.putText(image_np, '{}'.format(apx_distance), (int(mid_x * 800), int(mid_y * 450)),
-                           cv.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-
-                if (apx_distance / 1000) <= 0.9:
-                    if (mid_x / 1000) > 0.1 and (mid_x / 1000) < 0.9:
-                        cv.putText(image_np, 'WARNING!!!', (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 3)
 
 
 # capture and call needed functions
@@ -157,29 +122,13 @@ def start_processing():
         ret, frame = camera.read()
 
         orignal = frame.copy()
-        # DetectObject=
+
         ObjectDetector(frame)
-        # Detect_Distance(DetectObject[0],DetectObject[1],DetectObject[2],DetectObject[3])
-        # print(DetectObject[0])
-        # print(DetectObject[0][0])
 
-        # cv.imshow('oringal', orignal)
-
-        # # print(capture == True and counter < 10)
-        # if capture == True and counter < 10:
-        #     counter += 1
-        #     # cv.putText(
-        #     #     frame, f"Capturing Img No: {number}", (30, 30), fonts, 0.6, PINK, 2)
-        # else:
-        #     counter = 0
 
         cv.imshow('frame', frame)
         key = cv.waitKey(1)
 
-        # if key == ord('c'):
-        #     capture = True
-        #     number += 1
-        #     cv.imwrite(f'ReferenceImages/image{number}.png', orignal)
         if key == ord('q'):
             break
     cv.destroyAllWindows()
